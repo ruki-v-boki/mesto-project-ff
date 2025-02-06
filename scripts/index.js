20962210aeeda9ca7cc509b2322a7d48c359120e
ruki_v_boki <maksiimivanoov@yandex.ru>
2025-02-06 17:48:32 +0300

    first code

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,45 @@
-// @todo: Темплейт карточки
+'use strict'
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list')
+// получаем доступ к списку в который будут попадать карточки на странице
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function createCards(card, deleteCard) {
+    const template = document.querySelector('#card-template').content
+    // получаем доступ к шаблону
 
-// @todo: Вывести карточки на страницу
+    const cardItem = template.querySelector('.card').cloneNode(true)
+    // клонируем шаблон
+
+    const cardImage = cardItem.querySelector('.card__image')
+    const cardTitle = cardItem.querySelector('.card__title')
+    const cardDeleteButton = cardItem.querySelector('.card__delete-button')
+    // получаем доступ к узлам карточки внутри шаблона
+
+    cardImage.src = card.link
+    cardImage.alt = card.name
+    cardTitle.textContent = card.name
+    // наполняем узлы карточки контентом карточек из массива
+
+    cardDeleteButton.addEventListener('click', function () {
+        deleteCard(cardItem);
+    })
+    // добавляем кнопке обработчик клика и функцию-колбэк удаления карточки
+
+    return cardItem
+    // возвращаем готовую карточку
+}
+
+
+function deleteCard(card) {
+    card.remove()
+}
+    // функция удаления карточки
+
+
+
+initialCards.forEach(function (card) {
+    placesList.append(createCards(card, deleteCard));
+});
+    // добавляем каждую карточку в список на странице
+
