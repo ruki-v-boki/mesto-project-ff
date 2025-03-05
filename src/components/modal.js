f79be9a49331feb44ec323b2450f8a91c9d5f967
ruki_v_boki <maksiimivanoov@yandex.ru>
2025-03-05 05:16:15 +0300

    форма редактирования профиля

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,21 +1,28 @@
 export { openModal, closeModal, closeModalByOverlay, closeModalByKey }
 
 // функция открытия модального окна
-const openModal = modal => modal.classList.add('popup_is-opened')
+const openModal = (modal, nameInput, nameOutput, jobInput, jobOutput) => {
+    modal.classList.add('popup_is-opened')
+
+    if (modal.classList.contains('popup_type_edit')) {
+        nameInput.value = nameOutput.textContent
+        jobInput.value = jobOutput.textContent
+    }
+}
 
 // функция закрытия модального окна
 const closeModal = modal => modal.classList.remove('popup_is-opened')
 
 // функция-обработчик события клика по оверлею
-function closeModalByOverlay(event) {
-    if (event.target === event.currentTarget) {
-        event.currentTarget.classList.remove('popup_is-opened')
+function closeModalByOverlay(evt) {
+    if (evt.target === evt.currentTarget) {
+        evt.currentTarget.classList.remove('popup_is-opened')
     }
 }
 
 // функция-обработчик события нажатия Esc
-function closeModalByKey(event) {
-    if (event.key === 'Escape') {
+function closeModalByKey(evt) {
+    if (evt.key === 'Escape') {
         const modal = document.querySelector('.popup_is-opened')
         modal ? modal.classList.remove('popup_is-opened') : false
     }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,41 +5,67 @@ import { openModal, closeModal, closeModalByOverlay, closeModalByKey } from '../
 
 // профиль
 const profile = document.querySelector('.profile')
-const profileName = profile.querySelector('.profile__title').textContent
-const profileJob = profile.querySelector('.profile__description').textContent
+const profileName = profile.querySelector('.profile__title')
+const profileJob = profile.querySelector('.profile__description')
 
 // кнопки профиля
 const profileEditBtn = profile.querySelector('.profile__edit-button')
 const profileAddBtn = profile.querySelector('.profile__add-button')
 
 // слушатели кнопок профиля
-profileEditBtn.addEventListener('click', () => openModal(profileEditModal))
+profileEditBtn.addEventListener('click', () => {
+    openModal(profileEditModal, nameInput, profileName, jobInput, profileJob)
+})
+
 profileAddBtn.addEventListener('click', () => openModal(addNewCardModal))
 
 // модальные окна профиля
 const profileEditModal = document.querySelector('.popup_type_edit')
 const addNewCardModal = document.querySelector('.popup_type_new-card')
 
+//форма редактирования профиля
+const profileEditForm = document.forms['edit-profile']
+const nameInput = profileEditForm.querySelector('.popup__input_type_name')
+const jobInput = profileEditForm.querySelector('.popup__input_type_description')
+
+function handleFormSubmit(evt) {
+    evt.preventDefault()
+    profileName.textContent = nameInput.value
+    profileJob.textContent = jobInput.value
+    closeModal(profileEditModal)
+}
+
+profileEditForm.addEventListener('submit', handleFormSubmit)
+
+//форма создания новой карточки
+const addNewCardForm = document.forms['new-place']
+
+
+
+
+
+
 // все модальные окна
 const popups = document.querySelectorAll('.popup')
 
-    popups.forEach((popup) => {
-        popup.addEventListener('click', closeModalByOverlay)
-        document.addEventListener('keydown', closeModalByKey)
-        popup.classList.add('popup_is-animated')
-    })
+popups.forEach((popup) => {
+    popup.addEventListener('click', closeModalByOverlay)
+    document.addEventListener('keydown', closeModalByKey)
+    popup.classList.add('popup_is-animated')
+})
 
 //все кнопки закрытия модальных окон
 const closeModalButtons = document.querySelectorAll('.popup__close')
 
-    closeModalButtons.forEach((button) => {
-        const modal = button.closest('.popup')
-        button.addEventListener('click', () => closeModal(modal))
+closeModalButtons.forEach((button) => {
+    const modal = button.closest('.popup')
+    button.addEventListener('click', () => {
+        closeModal(modal)
     })
+})
 
 // контейнер карточек на странице
 const placesList = document.querySelector('.places__list')
 
 // вывод карточек на страницу
 initialCards.forEach((cardData) => placesList.append(createCard(cardData, deleteCard, openModal, toggleLike)))
-
